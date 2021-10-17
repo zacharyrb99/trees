f13a8e2f048e0cc7c0fba8d85495380df26c4fcc
zacharyrb99 <zach.boudreaux999@gmail.com>
2021-10-17 03:08:43 -0400

    finished trees

--- binary-tree.js
+++ binary-tree.js
@@ -17,58 +17,78 @@ class BinaryTree {
    * the length of the shortest path from the root to a leaf. */
 
   minDepth() {
+    if (!this.root) return 0;
 
+    const findMinDepth = (node) => {
+      if (node.left === null && node.right === null) return 1;
+      if (node.left === null) return findMinDepth(node.right) + 1;
+      if (node.right === null) return findMinDepth(node.left) + 1;
+      
+      return Math.min(findMinDepth(node.left), findMinDepth(node.right)) + 1;
+    }
+
+    return findMinDepth(this.root);
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
   maxDepth() {
+    if (!this.root) return 0;
 
+    const findMaxDepth = (node) => {
+      if (node.left === null && node.right === null) return 1;
+      if (node.left === null) return findMaxDepth(node.right) + 1;
+      if (node.right === null) return findMaxDepth(node.left) + 1;
+
+      return Math.max(findMaxDepth(node.left), findMaxDepth(node.right)) + 1;
+    }
+
+    return findMaxDepth(this.root);
   }
 
   /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
   maxSum() {
+    let total = 0;
 
+    const findMaxSum = (node) => {
+      if (node === null) return 0;
+      const leftSum = findMaxSum(node.left);
+      const rightSum = findMaxSum(node.right);
+
+      total = Math.max(total, node.val + leftSum + rightSum);
+
+      return Math.max(0, leftSum + node.val, rightSum + node.val);
+    }
+
+    findMaxSum(this.root);
+    return total;
   }
 
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
   nextLarger(lowerBound) {
+    if (!this.root) return null;
 
-  }
-
-  /** Further study!
-   * areCousins(node1, node2): determine whether two nodes are cousins
-   * (i.e. are at the same level but have different parents. ) */
-
-  areCousins(node1, node2) {
-
-  }
-
-  /** Further study!
-   * serialize(tree): serialize the BinaryTree object tree into a string. */
-
-  static serialize() {
-
-  }
-
-  /** Further study!
-   * deserialize(stringTree): deserialize stringTree into a BinaryTree object. */
-
-  static deserialize() {
-
-  }
-
-  /** Further study!
-   * lowestCommonAncestor(node1, node2): find the lowest common ancestor
-   * of two nodes in a binary tree. */
-
-  lowestCommonAncestor(node1, node2) {
+    let queue = [this.root];
+    let nextLargerVal = null;
     
+    while (queue.length) {
+      let currNode = queue.shift();
+      let currVal = currNode.val;
+      
+      (currVal > lowerBound && (currVal < nextLargerVal || !nextLargerVal)) 
+        ? nextLargerVal = currVal 
+        : null;
+        
+      if (currNode.left) queue.push(currNode.left);
+      if (currNode.right) queue.push(currNode.right);
+    }
+
+    return nextLargerVal;
   }
 }
 
--- tree.js
+++ tree.js
@@ -15,20 +15,67 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    if (!this.root) return 0;
+
+    let total = this.root.val;
+
+    const sumOfNodes = (node) => {
+      for (let child of node.children) {
+        total += child.val;
+
+        if (child.children.length !== 0) {
+          sumOfNodes(child);
+        }
+      }
+    }
+
+    sumOfNodes(this.root);
+    return total;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    if (!this.root) return 0;
 
+    let count;
+
+    this.root % 2 == 0 ? count = 1 : count = 0;
+
+    const isEven = (node) => {
+      for (let child of node.children) {
+        child.val % 2 == 0 ? count += 1 : count += 0;
+
+        if (child.children.length !== 0) {
+          isEven(child);
+        }
+      }
+    }
+
+    isEven(this.root);
+    return count;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    if (!this.root) return 0;
 
+    let count = this.root.val > lowerBound ? 1 : 0;
+
+    const isGreater = (node) => {
+      for (let child of node.children) {
+        child.val > lowerBound ? count++ : count += 0;
+
+        if (child.children.length > 0) {
+          isGreater(child);
+        }
+      }
+    }
+
+    isGreater(this.root);
+    return count;
   }
 }
 
